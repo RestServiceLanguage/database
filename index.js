a1443697571b16fc7bad76c459d0aca12a3fec3e
Moritz Schramm <moritz.schramm@gmx.de>
2019-11-18 19:45:58 +0100

    Add some default error classes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const ConstraintError = require('./lib/error/ConstraintError');
 const DatabaseAdapter = require('./lib/DatabaseAdapter');
 const KnexAdapter = require('./lib/KnexAdapter');
+const UnknownError = require('./lib/error/UnknownError');
 
 module.exports = {
+  ConstraintError,
   DatabaseAdapter,
-  KnexAdapter
+  KnexAdapter,
+  UnknownError
 };
