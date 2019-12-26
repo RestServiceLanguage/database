12e1bbd58400b179b8a8de381b8a67e298d1e7f0
Moritz Schramm <moritz.schramm@gmx.de>
2019-12-26 11:50:56 +0100

    Remove export of KnexAdapter

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const ConstraintError = require('./lib/error/ConstraintError');
 const DatabaseAdapter = require('./lib/DatabaseAdapter');
-const KnexAdapter = require('./lib/KnexAdapter');
 const UnknownError = require('./lib/error/UnknownError');
 
 module.exports = {
   ConstraintError,
   DatabaseAdapter,
-  KnexAdapter,
   UnknownError
 };
